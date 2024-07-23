d8b62beb7fb4d37f9a4029837b09daa6b914009c
rzvbka <wikstef2@gmail.com>
2024-07-23 23:06:29 +0200

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/mualaApp/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
