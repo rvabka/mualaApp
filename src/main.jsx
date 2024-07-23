b266b4fc9401d785fbe105393846e2c5aebc8676
rzvbka <wikstef2@gmail.com>
2024-07-23 22:51:40 +0200

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,35 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import "./styles/tailwind.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Layout from "./components/Layout";
-import Map from "./components/Map/Map";
-import MarkerDetail from "./components/Map/MarkerDetail";
-import List from "./components/List";
-
-const router = createBrowserRouter([
-  {
-    path: '/mualaApp/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Map />,
-      },
-      {
-        path: ':id',
-        element: <MarkerDetail />,
-      },
-      {
-        path: 'list',
-        element: <List />,
-      },
-    ],
-  },
-]);
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
